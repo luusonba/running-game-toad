eac89d39224b1ada9196d123445649e69e7bbeed
Le Minh Quan <luusonba@gmail.com>
2015-10-15 14:16:10 +0700

    [QuanLe] Update deploy

--- android/src/com/freeup/dino/runner/AndroidLauncher.java
+++ android/src/com/freeup/dino/runner/AndroidLauncher.java
@@ -1,6 +1,5 @@
 package com.freeup.dino.runner;
 
-import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
@@ -12,9 +11,6 @@ public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		if (getResources().getBoolean(R.bool.portrait_only)) {
-			setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-		}
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		initialize(new DinoRunner(), config);
 	}
